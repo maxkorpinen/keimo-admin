f70f05228d3d2fb7cf63f634d89cdeb5abb0b027
Max Korpinen <maxkorpinen@gmail.com>
2023-12-28 22:50:36 +0200

    db password reset

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import unitsRoute from './routes/unitsRoute.js'
+import unitsRoute from './routes/unitsRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy, allowing custom origins
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (request, response) => {
     console.log(request)
